a8df2ec93600d3fce65d6a65de1879c2ad18c48a
NamBV <nambv6886@gmail.com>
2025-10-02 10:10:58 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('Location Based Service Search System')
+  .setDescription('The location based service search system API description')
+  .setVersion('1.0')
+  .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 }
+
 bootstrap();
